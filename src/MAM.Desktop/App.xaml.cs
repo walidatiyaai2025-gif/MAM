6892a776708eff02e8b1f1476460dcb557080558
walidatiyaai2025-gif <walidatiyaai2025@gmail.com>
2026-09-12 19:10:36 +0300

    Fix WPF Application type qualification

--- src/MAM.Desktop/App.xaml.cs
+++ src/MAM.Desktop/App.xaml.cs
@@ -2,7 +2,7 @@ using System.Windows;
 
 namespace MAM.Desktop;
 
-public partial class App : Application
+public partial class App : System.Windows.Application
 {
     protected override void OnStartup(StartupEventArgs e)
     {
