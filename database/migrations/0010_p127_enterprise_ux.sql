d81ffe0b3e458e3ba054fb1053fbe142d54119f9
walidatiyaai2025-gif <walidatiyaai2025@gmail.com>
2026-09-15 09:06:19 +0300

    P12.7: capture extraction start time automatically

--- database/migrations/0010_p127_enterprise_ux.sql
+++ database/migrations/0010_p127_enterprise_ux.sql
@@ -6,6 +6,25 @@ BEGIN
     ALTER TABLE dbo.MamTextExtractionStatus ADD StartedAtUtc datetime2(7) NULL;
 END;
 
+EXEC(N'
+CREATE OR ALTER TRIGGER dbo.TR_MamTextExtractionStatus_P127StartedAt
+ON dbo.MamTextExtractionStatus
+AFTER INSERT, UPDATE
+AS
+BEGIN
+    SET NOCOUNT ON;
+    UPDATE target
+       SET StartedAtUtc = SYSUTCDATETIME()
+    FROM dbo.MamTextExtractionStatus target
+    JOIN inserted i
+      ON i.AssetId=target.AssetId AND i.ExtractionKind=target.ExtractionKind
+    LEFT JOIN deleted d
+      ON d.AssetId=i.AssetId AND d.ExtractionKind=i.ExtractionKind
+    WHERE i.State=N''''Running''''
+      AND (target.StartedAtUtc IS NULL OR d.CompletedAtUtc IS NOT NULL);
+END;
+');
+
 IF NOT EXISTS (SELECT 1 FROM dbo.MamSchemaVersion WHERE MigrationId=N'0010_p127_enterprise_ux')
     INSERT dbo.MamSchemaVersion(MigrationId) VALUES(N'0010_p127_enterprise_ux');
 
