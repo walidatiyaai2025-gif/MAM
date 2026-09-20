bf58b54fb703629d05cdbd63a72531e94536e984
walidatiyaai2025-gif <walidatiyaai2025@gmail.com>
2026-09-20 15:24:38 +0300

    Add Mac uploader App.cs

---
+++ src/MAM.MacUploader/App.cs
@@ -0,0 +1,23 @@
+using Avalonia;
+using Avalonia.Controls.ApplicationLifetimes;
+using Avalonia.Styling;
+using Avalonia.Themes.Fluent;
+
+namespace MAM.MacUploader;
+
+public sealed class App : Application
+{
+    public override void Initialize()
+    {
+        RequestedThemeVariant = ThemeVariant.Light;
+        Styles.Add(new FluentTheme());
+    }
+
+    public override void OnFrameworkInitializationCompleted()
+    {
+        if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
+            desktop.MainWindow = new MainWindow();
+
+        base.OnFrameworkInitializationCompleted();
+    }
+}
