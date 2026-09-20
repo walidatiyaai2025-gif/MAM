8ca7af8f75d8dbe7a88019d4257c8eebac2028dd
walidatiyaai2025-gif <walidatiyaai2025@gmail.com>
2026-09-20 15:24:35 +0300

    Add Mac uploader Program.cs

---
+++ src/MAM.MacUploader/Program.cs
@@ -0,0 +1,23 @@
+using Avalonia;
+
+namespace MAM.MacUploader;
+
+internal static class Program
+{
+    [STAThread]
+    public static void Main(string[] args)
+    {
+        if (args.Any(arg => string.Equals(arg, "--smoke-test", StringComparison.Ordinal)))
+        {
+            Console.WriteLine("DIWAN_MAM_MAC_UPLOADER_SMOKE_OK");
+            return;
+        }
+
+        BuildAvaloniaApp().StartWithClassicDesktopLifetime(args);
+    }
+
+    public static AppBuilder BuildAvaloniaApp() =>
+        AppBuilder.Configure<App>()
+            .UsePlatformDetect()
+            .LogToTrace();
+}
