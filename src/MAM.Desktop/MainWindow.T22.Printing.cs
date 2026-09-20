8de109cd556db1e112a9aef18d729949675a5b6c
walidatiyaai2025-gif <walidatiyaai2025@gmail.com>
2026-09-20 08:56:42 +0300

    Fix Desktop tape printing client namespace

--- src/MAM.Desktop/MainWindow.T22.Printing.cs
+++ src/MAM.Desktop/MainWindow.T22.Printing.cs
@@ -2,6 +2,7 @@ using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Media;
 using System.Windows.Shapes;
+using MAM.Application.Clients;
 using MAM.Application.Tapes;
 
 namespace MAM.Desktop;
